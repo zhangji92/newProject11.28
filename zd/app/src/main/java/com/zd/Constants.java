a48ba427ae7d19bab7b34827e4e585b038ad7946
zhangji92 <938361820@qq.com>
2016-11-30 09:12:22 +0800

    微信支付

---
+++ zd/app/src/main/java/com/zd/Constants.java
@@ -0,0 +1,22 @@
+package com.zd;
+
+public class Constants {
+
+
+    //appid
+    //请同时修改  androidmanifest.xml里面，.PayActivityd里的属性<data android:scheme="wxb4ba3c02aa476ea1"/>为新设置的appid
+  public static final String APP_ID = "wxf04db53eea7c8667";
+
+
+
+
+  //商户号
+   public static final String MCH_ID = "1381253902";
+
+
+//  API密钥，在商户平台设置
+    public static final  String API_KEY="lwt66666666666666666666666666666";
+
+
+
+}
